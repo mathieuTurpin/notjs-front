ac528ecda99ff8a6b45918c1765de5177d5968f7
mathieu <mathieu.turpin974@hotmail.com>
2013-12-05 13:38:35 +0100

    install ejs-locals and add default layout

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 var express = require('express');
 var app = express();
 
+var engine = require('ejs-locals');
+ 
+app.configure(function(){
+	app.set('views', __dirname+'/views');
+	app.engine('ejs', engine);
+});
+
 app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+	res.setHeader("Content-Type", "text/html");
+	var data = {
+		title: 'Accueil Team not.js',
+		layoutFile: 'layout.ejs'
+	};
+    res.render('index.ejs',data);
+});
+
+app.use(function(req, res, next){
+    res.setHeader('Content-Type', 'text/plain');
+    res.send(404, 'Page introuvable !');
+})
 
 app.listen(process.env.PORT || 5000);
